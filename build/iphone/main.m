2d307a4da8ba9a65555a110f4ee7d6f5b039e4cc
Justin Howard <info@knightfinderapp.com>
2011-09-11 20:29:34 +0100

    2.0.2

--- build/iphone/main.m
+++ build/iphone/main.m
@@ -7,7 +7,7 @@
 #define _QUOTEME(x) #x
 #define STRING(x) _QUOTEME(x)
 
-NSString * const TI_APPLICATION_DEPLOYTYPE = @"test";
+NSString * const TI_APPLICATION_DEPLOYTYPE = @"production";
 NSString * const TI_APPLICATION_ID = @"com.knightfinderapp";
 NSString * const TI_APPLICATION_PUBLISHER = @"Knight Application Systems";
 NSString * const TI_APPLICATION_URL = @"http://www.knightfinderapp.com";
